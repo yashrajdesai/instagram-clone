4f8bba46640fd2fef68d33d8963a64d342fff09b
yashraj <yashrajdesai30@gmail.com>
2020-08-31 22:25:59 +0530

    Authentication modified

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports= {
     MONGOURI : process.env.MONGOURI,
     JWT_SECRET: process.env.JWT_SEC,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
+    // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    // GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET
 };
